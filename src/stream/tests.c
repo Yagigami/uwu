feb752c3abdfe4915aedb2df19021da6f69c49b1
yagigami <yagigami.goatgod@gmail.com>
2020-05-19 23:43:09 +0200

    return value for stream test

--- src/stream/tests.c
+++ src/stream/tests.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void stream_test(void) {
+int stream_test(void) {
 	printf("stream:\n");
 	Stream stream = stream_init("foo.c", C_STREAM_READ | C_STREAM_TEXT);
 	if (!stream) {
@@ -23,5 +23,6 @@ void stream_test(void) {
 		free(line);
 	}
 	stream_fini(stream);
+	return 0;
 }
 
