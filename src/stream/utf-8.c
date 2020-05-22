7d2d306b160a1157efcb807904806ed35854e872
yagigami <yagigami.goatgod@gmail.com>
2020-05-22 20:24:03 +0200

    it looks smart uwu

--- src/stream/utf-8.c
+++ src/stream/utf-8.c
@@ -41,7 +41,9 @@ bool is_valid_character_utf_8(const uint8_t *strm, long len, uint8_t **endptr) {
 
 bool is_valid_buffer_utf_8(const uint8_t *strm, long len) {
 	for (const uint8_t *s = strm, *end = strm + len; s != end;) {
-		if (!is_valid_character_utf_8(s, end - s, &s)) return false;
+		uint8_t *out;
+		if (!is_valid_character_utf_8(s, end - s, &out)) return false;
+		s = out;
 	}
 	return true;
 }
@@ -79,25 +81,39 @@ uint32_t codepoint_utf_8(const uint8_t *strm, uint8_t **endptr) {
 }
 
 int encode_utf_8(uint8_t *strm, uint32_t cp, uint8_t **endptr) {
-	int ret = len_character_utf_8(cp);
-	if        (cp <= 0x00007F) {
-		strm[0] = cp;
-	} else if (cp <= 0x0007FF) {
-		strm[0] = 0xC0 | ((cp >>  6) & 0x1F);
-		strm[1] = 0x80 | ((cp >>  0) & 0x3F);
-	} else if (cp <= 0x00FFFF) {
-		strm[0] = 0xE0 | ((cp >> 12) & 0x0F);
-		strm[1] = 0x80 | ((cp >>  6) & 0x3F);
-		strm[2] = 0x80 | ((cp >>  0) & 0x3F);
-	} else if (cp <= 0x10FFFF) {
-		strm[0] = 0xF0 | ((cp >> 18) & 0x07);
-		strm[1] = 0x80 | ((cp >> 12) & 0x3F);
-		strm[2] = 0x80 | ((cp >>  6) & 0x3F);
-		strm[3] = 0x80 | ((cp >>  0) & 0x3F);
+	int len;
+	unsigned or, msk, shamt;
+	if      (cp <= 0x00007F)
+		len = 1, or = 0x00, msk = 0xFF;
+	else if (cp <= 0x0007FF)
+		len = 2, or = 0xC0, msk = 0x1F;
+	else if (cp <= 0x00FFFF)
+		len = 3, or = 0xE0, msk = 0x0F;
+	else if (cp <= 0x10FFFF)
+		len = 4, or = 0xF0, msk = 0x07;
+	else
+		__builtin_unreachable();
+	shamt = (len - 1) * 6;
+	switch (len) {
+	case 4:
+		*strm++ = or | ((cp >> shamt) & msk);
+		shamt -= 6, or = 0x80, msk = 0x3F;
+		/* fallthrough */
+	case 3:
+		*strm++ = or | ((cp >> shamt) & msk);
+		shamt -= 6, or = 0x80, msk = 0x3F;
+		/* fallthrough */
+	case 2:
+		*strm++ = or | ((cp >> shamt) & msk);
+		shamt -= 6, or = 0x80, msk = 0x3F;
+		/* fallthrough */
+	case 1:
+		*strm++ = or | ((cp >> shamt) & msk);
+		shamt -= 6, or = 0x80, msk = 0x3F;
+		/* fallthrough */
 	}
-	strm += ret;
 	if (endptr) *endptr = (uint8_t *) strm;
-	return ret;
+	return len;
 }
 
 int len_character_utf_8(uint32_t cp) {
